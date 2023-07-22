94a0131fffa7ccbadd0a2184883cdb9b516ad0b4
Tushar Khande <90444989+TusharKhande98@users.noreply.github.com>
2023-07-22 22:13:09 +0530

    Add files via upload

---
+++ QSP13_Pattern1_Square.java
@@ -0,0 +1,13 @@
+class QSP13_Pattern1_Square {
+    public static void main(String[] args) {
+
+        for (int i=0; i<4; i++)
+        {
+            for (int k=0; k<4; k++)
+            {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
