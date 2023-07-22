bf0ce768b2987362e188a3118aba3d51b4acf777
Tushar Khande <90444989+TusharKhande98@users.noreply.github.com>
2023-07-22 13:34:28 +0530

    Add files via upload

---
+++ QSP12_NestedLoop_Intro.java
@@ -0,0 +1,15 @@
+class QSP12_NestedLoop_Intro {
+    public static void main(String[] args) {
+
+        for (int i=0; i<2; i++)
+        {
+            for (int k=0; k<3; k++)
+            {
+                System.out.println("Intro:- NestedFor loop");
+            }
+            System.out.println("-------------------------");
+        }
+        System.out.println("\n3 line has been printed in 1 execution\nTotal outer execution are 2 & inner execution are 3...\n\tTherefore total lines are 3x2=6");
+        System.out.println("*- Observe the output to understand it -*");
+    }
+}
