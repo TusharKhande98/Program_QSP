70122de50eafa0a58293d4811572d8fe62d2529f
Tushar Khande <90444989+TusharKhande98@users.noreply.github.com>
2023-09-14 08:38:48 +0530

    Add files via upload

---
+++ QSP25_Method_Ex1.java
@@ -0,0 +1,11 @@
+class QSP25_Method_Ex1 {
+    public static void main(String[] args) {
+        System.out.println("Topic name:- Method");
+        callMethod();
+    }
+
+    private static void callMethod()
+    {
+        System.out.println("Hi... I'm user defined method named as -->'callMethod()'.\nAnd I was called from main method. ");
+    }
+}
