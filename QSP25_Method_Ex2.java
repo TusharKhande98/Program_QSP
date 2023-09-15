0966016e5452e767ce89eeadace40d88035c2a24
Tushar Khande <90444989+TusharKhande98@users.noreply.github.com>
2023-09-15 08:36:38 +0530

    Add files via upload

---
+++ QSP25_Method_Ex2.java
@@ -0,0 +1,21 @@
+class QSP25_Method_Ex2
+{
+    public static void main(String[] args)
+    {
+        boyFriend();
+        boyFriend();
+        boyFriend();
+        girlFriend();
+        boyFriend();
+    }
+
+    private static void girlFriend()
+    {
+        System.out.println("Get Lost!");
+    }
+
+    private static void boyFriend()
+    {
+        System.out.println("Hey...");
+    }
+}
